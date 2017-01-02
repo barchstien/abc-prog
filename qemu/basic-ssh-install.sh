bb7e5da2c52f54492afcd178437f6b4618172a64
bastien <bastien.auneau@while-true.fr>
2017-01-02 16:55:12 +0100

    forgot about nographic agin...

--- qemu/basic-ssh-install.sh
+++ qemu/basic-ssh-install.sh
@@ -14,7 +14,7 @@ qemu-img create -f qcow2 disk.img 20G
 qemu-system-x86_64 -enable-kvm -drive file=disk.img,format=qcow2 -m 2G -boot d -cdrom /path/to/ubuntu-16.04.1-server-amd64.iso
 
 #run system without graphic, with ssh port (only) redirected to localhost
-qemu-system-x86_64 -enable-kvm -drive file=disk.img,format=qcow2 -m 2G -redir tcp:2222::22
+qemu-system-x86_64 -enable-kvm -drive file=disk.img,format=qcow2 -m 2G -redir tcp:2222::22 -nographic
 
 #from host system, can now login with
 ssh -p 2222 user@localhost
