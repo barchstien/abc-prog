b2b55119dc3f05643893f931bd7ded917ad032aa
bastien <barchstien@users.noreply.github.com>
2017-02-10 12:28:52 +0100

    replace -nographics with -display none for running

--- qemu/run_vm.sh
+++ qemu/run_vm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 port=2223
-echo "ssh local port : $port  (bash args are append)"
-cmd="qemu-system-x86_64 -enable-kvm -drive file=fedora19_stapisdk.qcow2,format=qcow2 -m 2G -nographic -redir tcp:$port::22 $*"
+echo "ssh local port : $port  (bash args are append, -smp n for n proc)"
+cmd="qemu-system-x86_64 -enable-kvm -drive file=fedora19_stapisdk.qcow2,format=qcow2 -m 2G -daemonize -display none -redir tcp:$port::22 $*"
 echo $cmd
 $cmd
